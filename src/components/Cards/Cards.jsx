c141f2759fce560b5db5e861402cc55736f214b7
kristijan-kresic-hvar <cropowzed@gmail.com>
2020-04-21 22:46:07 +0200

    Initial Commit

---
+++ src/components/Cards/Cards.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Cards = () => {
+    return <h1>Hello from Cards</h1>
+}
+
+export default Cards;
---
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CountryPicker = () => {
+    return <h1>Hello from Country Picker</h1>
+}
+
+export default CountryPicker;
